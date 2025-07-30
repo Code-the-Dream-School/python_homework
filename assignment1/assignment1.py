c5b1d990c17993412c12aaf5db1b4c744bb6582d
Lydia B <lydiabizuneh7@gmail.com>
2025-07-29 17:41:06 -0700

    assignment1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,1 +1,19 @@
-# Write your code here.
+#Task 1
+def hello():
+    return "Hello, World!"
+#Task 2
+def greet(name):
+    return f"Hello, {name}!"
+#Task 3
+def calc(x,y,operator):
+   match operator:
+       case 'add':
+           return x + y
+       case 'subtract':
+            return x - y
+       case 'multiply':
+            return x * y
+       case 'divide':
+                return x / y
+         
+calc(10, 5, 'add')  # Example usage
